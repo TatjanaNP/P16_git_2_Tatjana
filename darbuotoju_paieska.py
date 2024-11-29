0467a0f8a56d57a2132c0364cbd4ac65cd9a0030
TatjanaNP <tatjananpetrova@outlook.com>
2024-11-29 09:26:49 +0200

    Pridetas netinkamu kandidatu sarasas darbuotoju paieskos faile

--- darbuotoju_paieska.py
+++ darbuotoju_paieska.py
@@ -1,6 +1,9 @@
 def atranka(kandidatu_sarasas: dict[str: list[str | int]], pareigos: str, atlyginimo_riba) -> list[str]:
     tinkami_kandidatai = []
+    netinkami_kandidatai = []
     for key, value in kandidatu_sarasas.items():
         if value[0] == pareigos and value[1] <= atlyginimo_riba:
             tinkami_kandidatai.append(key)
+        else:
+            netinkami_kandidatai.append(key)
     return tinkami_kandidatai
